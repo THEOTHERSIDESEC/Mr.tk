fe7a93610261a80529dcee430fd6d4b8acfc085b
The Otherside <80984256+THEOTHERSIDESEC@users.noreply.github.com>
2021-03-27 15:12:10 +0200

    Add files via upload

---
+++ Mrstart.sh
@@ -0,0 +1,71 @@
+# A tool by versacefer  from The otherside
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#-----------------
+# scripting starts
+clear
+pkg install toilet -y
+pkg install lolcat -y
+echo toilet -f big ' DDOS ' -F gay | lolcat 
+echo " " 
+
+echo -e "$red                      ▶ Coded by$grn Versacefer $red ◀$rset"
+echo -e "$red                     ⫸$ylo  The Otherside $red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                   [Destroy Them ] $rset"
+echo " "
+echo " "
+echo -e "$grn              ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ☠$cyan [1] Start Ddos"
+echo -e "$red                        ☠$cyan [2] Update script"
+echo -e "$red                        ☠$cyan [3] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -e "$cyan                   [[[$ylo Select any option$cyan]]]$rset"
+echo " "
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo " "
+echo -e "$red               You are going to$grn start the tool$red Now$rset"
+echo " "
+echo -e "$grn                  Press$ylo ENTER$red to continue$rset"
+echo " "
+cd Mr.tk , python2 Tk.py
+echo " "m
+2)echo " "
+echo " "
+echo " "
+echo -e "$red             You are going to$grn Update the$red Script$rset "
+echo " "
+echo -e "$grn                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+rm -rf Mr.tk , cd $HOME , git clone htts://github.com/THEOTHERSIDESEC/Mr.TK , bash Mrstart.sh
+echo " "
+3)
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+     ____  _  _  ____    ____  _  _  ___
+    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+     ) _ < \  /  )__)    ) _ < \  /  )__)
+    (____/ (__) (____)  (____/ (__) (____)...$cyan have a awesome day$rset"
+
+echo " "
+echo " "
+esac

f4b0c3c068964df95b86b311eb41c5e37fa91bc0
The Otherside <80984256+THEOTHERSIDESEC@users.noreply.github.com>
2021-03-27 15:20:58 +0200

    Add files via upload

---
+++ Mr.tk/banner.py
@@ -0,0 +1,36 @@
+#!/bin/bash
+print("""\033[97m
+                                       			 
+                      .:/++++++++++/:.                      
+                 `/+++:.            .:+++/.                 
+              .+o/.                      ./o+.              
+            -o+`                            `/o:            
+          -s/               ````               :s-          
+        `o+   .++.    `..--.-..-.--..`    .++.   +o`        
+       .y- :/sm+   `--`..` -`..`- `..`--`   +ms/: -y.       
+      .y.`+d:so: `.` `-....--/+/:....-` `.` :os:ho`.y.      
+      h.:/dsyy- .`   -`   - +/+m--   `-   `. -yysd//.h      
+     ++ moyo+: -..``.`   `.  .+` .`   `. `..- -+oy+m`++     
+    `d .msom+ -`   `-....-...oo...-....-.`  `- +mosm. d`    
+    :s`oohh/.`-    ..    -  `--`  -    ..    -`./hhoo`s:    
+    ++.m-s:d``.````..````::/:yh:/::````..````..`d:s-m.++    
+    ++ hm-mo `.````..+syhmm-`oo`-mmhys+..````.. om-dh ++    
+    :s./yhd-+`-    .smmmmmm. yy .mmmmmms.    -`+-dhy/.s:    
+    `d`ho/ooh .`  `.dmmmmmmy hh smmmmmmd.`  `- hoo+oh`d`    
+     ++`ym+sm.:-..`.mmmmmmmmyddymmmmmmmm.``.-:.ms+my`++     
+      h.-/shm-d/.` :mmmmmmmmmmmmmmmmmmmm/ `./d-mhs/-.h      
+      `y.+y++/om-:.smmmmmmmmmmmmmmmmmmmmy.:-mo/++y+.y.      
+       `y-`+hdshh/yymmmmmmmmmmmmmmmmmmmmyy/hhsdh+.-y.       
+        `oo`-+++oo:ymmmmmmmmmmmmmmmmmmmmy:oo+++-`+o`        
+          -s/`:oyhhhsosdmmmmmmmmmmmmdsoshhhyo:`/s-          
+            -o+.:+sydNMmmmmmmmmmmmmmNMNdys+:.+o-            
+              `+o+.``` dmmmmmmmmmmmmd ```./o+.              
+                 `:+++:mmmmmmmmmmmmmm:+++/`                 
+                      .:/ossyyyyyso+:.                          
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Author     : Versacefer
+Team       : The otherside
+Thanks to  : The Otherside
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+TOOL DDOS  ATTACK                                         """)

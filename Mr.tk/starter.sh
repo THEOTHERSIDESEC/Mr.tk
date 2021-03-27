12625dad580c305a9e2c8def761e6067d6274e24
The Otherside <80984256+THEOTHERSIDESEC@users.noreply.github.com>
2021-03-27 16:18:02 +0200

    Add files via upload

---
+++ Mr.tk/starter.sh
@@ -0,0 +1,7 @@
+# A tool by versacefer  from The otherside
+# scripting starts
+
+echo toilet -f big ' Message' -F gay | lolcat 
+echo -e "$red   Greetings user please use a vpn$ ◀$rset" 
+sleep 4.0
+python2 tk.py
